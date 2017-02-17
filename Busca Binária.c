e6907f2f4521f57f3ca4c0dd8aebe54455c91a39
Danrley <danrleyhenrique2010@hotmail.com>
2017-02-17 19:22:18 -0300

    Update Busca Binária.c

--- Busca Binária.c
+++ Busca Binária.c
@@ -1,63 +1,71 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<time.h>
+ 
 
 
 int geradorAleatorio()
 {
 	//Essa função é capaz de gerar um número aleatório
 	int x;
-	x = rand() %  200;
+	x = rand() %  4;
 	if((rand() % 3) == 1)
 		return x*(-1);
 	else
 		return x;	
 }
 
-int PesquisaBinaria ( int vet[], int chave, int Tam)
-{
-     int inf = 0;     //Limite inferior  (o primeiro elemento do vetor em C é zero          )
-     int sup = Tam-1; //Limite superior  (termina em um número a menos 0 a 9 são 10 numeros )
-     int meio;
-     while (inf <= sup)
-     {
-          meio = (inf + sup)/2;
-          if (chave == vet[meio])
-               return meio;
-          if (chave < vet[meio])
-               sup = meio-1;
-          else
-               inf = meio+1;
-     }
-     return -1;   // não encontrado
-}
 
 int main(){
-  int chave, Tam, j, expoente, i;
-  chave = 45;
-  Tam = 10;
-  
-  ////////////// For para gerar a exponenciação
-	j = 1;
-	
-	expoente = 3;
+  double tempoInicial, tempoFinal, tempoGasto;
+  setlocale(LC_ALL,""); srand((unsigned) time(NULL));
+  int j, i; short int expoente;
+  j = 1; expoente = 1;
 	
 	for(i = 0; i < expoente; i++){
 		j = j * 2;
-	}
-	
-	////////////// For para semear o Array
-	int Vetor[j];
-	i = rand() % j;
-	chave = Vetor(i);
-	
-	for(i = 0; i < j; i++){
-		Vetor[i] = geradorAleatorio();
-		printf("%d ", Vetor[i]);	
-	}
+}  
   
-  printf(" O elemento %d está no indice %d", chave, PesquisaBinaria(Vetor, chave, Tam));
-  return 0;
+
+int v[j-1];
+int x,inicio,meio , fim ;
+int num; //número a ser buscado.
+
+for(x=0;x<j;x++)
+{ 
+  v[x] = geradorAleatorio();
+  printf(" %d ", v[x]);
+}
+num = v[j-1];
+inicio=0;
+fim = sizeof(v);
+fim = fim/4;
+ 
+ 
+tempoInicial = clock(); 
+if(v[inicio]==num){
+printf("\n numero %d encontrado no indice %d\n",num, inicio);
+}
+else if(v[fim]==num){
+printf("\n numero %d encontrado no indice %d\n", num, fim);
+}
+else{
+  while(inicio<fim){ 
+    meio=(inicio+fim)/2; //aqui no caso estou fazendo um typecasting, irei truncar o valor do quociente para pegar somente a parte inteira.
   
-  
+    if ( v[meio]==num){ 
+    printf("\n numero %d encontrado no indice %d\n", num, meio);break;
+    }else 
+      if(num>v[meio]){ 
+       inicio=meio+1;
+      }else{ 
+        fim=meio;
+     }
+   }
+  }
+  tempoFinal = clock();
+  tempoGasto = (tempoFinal - tempoInicial) * 1000 / CLOCKS_PER_SEC;
+	printf("\nTempo em milisegundos: %0.11fms", tempoGasto);
+return 0;
 }
